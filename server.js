27eac8549514e3357c15e33e4ef1bf1aed5d3d78
SteveObee <stephenobee1@gmail.com>
2019-10-01 15:16:06 +0100

    Pre deploy

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, 'build')));
+}
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
+const PORT = process.env.PORT || 9000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
